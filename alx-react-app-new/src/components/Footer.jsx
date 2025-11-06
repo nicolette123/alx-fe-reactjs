23c4ad04a6aa4390e167d6b2f23dc3e73830f436
Nicolette <nnicky8902gmail.com>
2025-11-06 15:32:14 +0200

    changed extension

--- alx-react-app-new/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: 'lightgray', color: 'black', textAlign: 'center', padding: '10px' }}>
-      <p>© 2023 City Lovers</p>
-    </footer>
-  );
-}
-
-export default Footer;
