671be76442a7a3e976cb21df953f7fe82c1a17a1
Nicolette <nnicky8902gmail.com>
2025-11-07 11:58:35 +0200

    started add inline styles

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#f5f5f5', padding: '20px', textAlign: 'center' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
